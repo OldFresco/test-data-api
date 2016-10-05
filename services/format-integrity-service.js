a67737dc889a61c325e5fd0668f8eadfed4a7026
OldFresco <hinsonkojo@gmail.com>
2016-10-05 21:25:59 +0100

    add some more queries and make a few slight tweaks

--- services/format-integrity-service.js
+++ services/format-integrity-service.js
@@ -1,6 +1,6 @@
 module.exports = {
-    isValid: function (query) {
-        if (query === 'customer-with-good-credit') {
+    isValid: function(query) {
+        if (query.length > 0) {
             return true;
         }
         return false;
--- services/test-data-query-service.js
+++ services/test-data-query-service.js
@@ -1,14 +1,30 @@
 module.exports = {
-    execute: function (query) {
+    execute: function(query) {
         var testData = {
             data: {
                 firstName: "Test",
-                lastName: "Case"
+                lastName: "Case",
+                credit: "good"
             },
             exists: true
         };
 
         if (query === 'customer-with-good-credit') {
+            return testData;
+        };
+
+        if (query === 'customer-with-bad-credit') {
+            testData.data.credit = 'bad';
+            return testData;
+        };
+
+        if (query === 'customer-with-dodgy-first-name') {
+            testData.data.firstName = '???';
+            return testData;
+        };
+
+        if (query === 'customer-with-dodgy-last-name') {
+            testData.data.lastName = '???';
             return testData;
         };
 
