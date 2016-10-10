d2ec20cdf38ea0b594f7fce315c9c8a50c430eda
Kojo Hinson <kojo.hinson@oakbrook.local>
2016-10-10 01:19:39 +0100

    Minor code changes

--- services/request-processing-service.js
+++ services/request-processing-service.js
@@ -1,23 +1,47 @@
+//Load up service dependencies
+var securityGuard = require('authorization-service');
+var receptionist = require('request-integrity-service');
+var processingManager = require('request-interpretation-service');
+
+//Define pre processing steps
 var preProcessing = function(req) {
-    if (!authorizationService.isSecure(req)) {
-        return authorizationService.failedAuthorizationResponse;
+    outcome = {
+        content: null,
+        error: true
+    };
+
+    //Step 1. Check if we recognize source ID. Stamp with session ID and source ID if required. 
+    securityGuard.capturesRequestIdentity(req);
+
+    //Step 2. Check if request meets access criteria and/or security rules
+    if (!securityGuard.Secures(req)) {
+        outcome.content = securityGuard.failedAuthorizationResponse;
+        return outcome;
     }
 
-    authorizationService.captureRequestIdentity(req);
-
-    if (!requestIntegrityService.isValid(req)) {
-        return requestIntegrityService.badlyFormattedRequestResponse;
+    //Step 3. Check request is in an understandable format 
+    if (!receptionist.understands(req)) {
+        outcome.content = receptionist.failedAuthorizationResponse;
+        return outcome;
     }
 
-    return req;
+    outcome.content = req;
+    outcome.error = false;
+
+    return outcome;
 }
 
 module.exports = {
+    // Execute main processing of request 
     Process: function(req) {
-        var executionReadyRequest = preProcessing(req);
+        var preProcessingOutcome = preProcessing(req);
 
-        response = requestInterpreter.execute(executionReadyRequest);
+        if (!preProcessingOutcome.error) {
+            response = processingManager.execute(preProcessingOutcome);
 
-        return response;
+            return response;
+        }
+
+        return preProcessingOutcome.content;
     }
 };
